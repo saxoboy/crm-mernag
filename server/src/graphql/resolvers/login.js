b47d67d4a9dfb0e3ac422410aa048085e0bb7b05
Israel Herrera <israelherrerae@gmail.com>
2020-05-03 15:59:17 -0500

    login por default

--- server/src/graphql/resolvers/login.js
+++ server/src/graphql/resolvers/login.js
@@ -1,32 +1,40 @@
-import bcrypt from 'bcrypt';
-import { getToken } from '../../config/middleware/auth';
+import bcrypt from "bcrypt";
+import { getToken } from "../../config/middleware/auth";
 
 export default {
-    Mutation: {
-        async login(root, { input }, { models: { user } }) {
-            let { email, password } = input;
-            try {
-                let User = await user.findOne({ email }, 'id role name username email password');
+  Mutation: {
+    async login(root, { input }, { models: { user } }) {
+      let { email, password } = input;
+      try {
+        let User = await user.findOne(
+          { email },
+          "name password role" //"id role name username email password"
+        );
 
-                if (User && bcrypt.compareSync(password, User.password)) {
-                    return {
-                        ok: true,
-                        message: 'Authenticated',
-                        me: { user: User.id, role: User.role, name: User.name, username: User.username, email: User.email },
-                        token: getToken({ user: User.id, role: User.role })
-                    };
-
-                } else return {
-                    ok: false,
-                    message: 'Incorrect email or password'
-                };
-
-            } catch (err) {
-                return {
-                    ok: false,
-                    message: err.message
-                };
-            }
-        }
-    }
+        if (User && bcrypt.compareSync(password, User.password)) {
+          return {
+            ok: true,
+            message: "Authenticated",
+            token: getToken({ user: User.id, role: User.role }),
+            // me: {
+            //   user: User.id,
+            //   role: User.role,
+            //   name: User.name,
+            //   username: User.username,
+            //   email: User.email,
+            // },
+          };
+        } else
+          return {
+            ok: false,
+            message: "Incorrect email or password",
+          };
+      } catch (err) {
+        return {
+          ok: false,
+          message: err.message,
+        };
+      }
+    },
+  },
 };
